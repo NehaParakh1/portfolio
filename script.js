eeb7a6b6082a672f60fa1d4625cfb1cbe3bccd87
NehaParakh1 <nehadugar3@gmail.com>
2023-07-13 14:14:01 +0530

    making responsive

--- script.js
+++ script.js
@@ -5,4 +5,24 @@ var typed= new Typed(".text",{
     backDelay:1000,
     loop:true
 })
+const menuIcon = document.querySelector('.menu-icon');
+const navItems = document.querySelector('.nav-items');
+const navLinks = document.querySelectorAll('.nav-items');
 
+menuIcon.addEventListener('click', () => {
+  navItems.classList.toggle('open');
+});
+
+navLinks.forEach(navLink => {
+  navLink.addEventListener('click', () => {
+    navItems.classList.remove('open');
+  });
+});
+
+document.addEventListener('click', (event) => {
+  const target = event.target;
+  
+  if (!target.classList.contains('menu-icon') && !navItems.contains(target)) {
+    navItems.classList.remove('open');
+  }
+});
